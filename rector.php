fcb0c6abad36cc0bbdb21505bfb71be3cbd647b0
Philippe Delente <philippe.delente@gmail.com>
2025-05-28 18:16:50 +0200

    feat(chore): update symfony 7.3-rc + update config rector + update ci

--- rector.php
+++ rector.php
@@ -3,26 +3,23 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src',
+return RectorConfig::configure()
+    ->withSets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
+        SetList::PHP_83,
+        SetList::RECTOR_PRESET,
+    ])
+    ->withComposerBased(twig: true, doctrine: true, phpunit: true, symfony: true)
+    ->withPaths([
+        __DIR__.'/src',
+    ])
+    ->withSkip([
+        '*/src/Entity/*',
+        '*/src/Repository/*',
     ]);
-
-    $rectorConfig->phpVersion(PHP_VERSION_ID);
-
-    $rectorConfig->sets([
-        SymfonySetList::SYMFONY_72,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        LevelSetList::UP_TO_PHP_83,
-
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/src/Kernel.php',
-        __DIR__ . '/src/Entity',
-    ]);
-};
